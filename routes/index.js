c06cfb11b04004fbb4bcfcb9cf90ab240ee7862c
Niko <onescyt@gmail.com>
2017-02-18 17:24:03 +1100

    stores api payload in memory cache for 5 minutes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var cache = require('memory-cache');
 
 var getContent = function() {
   return new Promise((resolve, reject) => {
@@ -8,16 +9,26 @@ var getContent = function() {
       if (err) {
         reject(err); return;
       }
+      cache.put('content', body, (5 * 60 * 1000)); // store it in memory cache for 5 minutes
       resolve(body);
     });
   });
 }
 
 var getContentMiddleware = function(req, res, next) {
-	var content = getContent().then(function(json) {
-		res.locals.content = json;
-		next();	
-	});	
+  var cachedContent = cache.get('content');
+ 
+  if (cachedContent !== null) {
+    res.locals.content = cachedContent;
+    next();
+  } else {
+    getContent().then(function(json) {
+      res.locals.content = json;
+      next();
+    });
+  }
+
+ 
 }
 
 router.get('/', getContentMiddleware, function(req, res, next) {
